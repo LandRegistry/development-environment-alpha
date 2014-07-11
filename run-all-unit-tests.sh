50db114a63a0f89b06db2594b6625d94179b027a
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-07-11 16:04:05 +0100

    Added script to run all unit tests

---
+++ run-all-unit-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+script/run-all-unit-tests.sh $@
---
+++ script/run-all-unit-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ ! -d ./apps ]] ; then
+	./bootstrap
+fi
+
+source script/dev/configure
+source script/checkout
+ 
+apps_and_ports=`cat script/apps | grep -v \# | grep -v '^\s*$'`
+
+for app_and_port in ${apps_and_ports}; do
+	app=`echo ${app_and_port} | cut -d':' -f1`
+
+	echo "Running unit tests for ${app}"
+
+	cd apps/${app}
+	dev/run-unit-tests
+	cd ../..
+done
